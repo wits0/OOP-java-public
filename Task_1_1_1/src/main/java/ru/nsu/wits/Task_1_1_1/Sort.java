2c62db1fe771bc68fd4159fc6d96ebd8bd890af4
wits0 <v.bobrovnikov@g.nsu.ru>
2026-09-07 12:06:43 +0700

    fix checkstyle AGAIN !!!

--- Task_1_1_1/src/main/java/ru/nsu/wits/Task_1_1_1/Sort.java
+++ Task_1_1_1/src/main/java/ru/nsu/wits/Task_1_1_1/Sort.java
@@ -1,7 +1,17 @@
 package ru.nsu.wits.Task_1_1_1;
 
+/**
+ * Utility class for sorting arrays.
+ */
 public class Sort {
-    public static int[] sort(int[] array){
-        return new int[]{1,2,3};
+
+    /**
+     * Sorts the given array in ascending order.
+     *
+     * @param array the input array to be sorted
+     * @return a new sorted array
+     */
+    public static int[] sort(int[] array) {
+        return new int[]{1, 2, 3};
     }
 }
--- Task_1_1_1/src/test/java/ru/nsu/wits/Task_1_1_1/SortTest.java
+++ Task_1_1_1/src/test/java/ru/nsu/wits/Task_1_1_1/SortTest.java
@@ -1,15 +1,14 @@
 package ru.nsu.wits.Task_1_1_1;
 
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 class SortTest {
 
     @Test
     void sort() {
-        int[] array = new int[]{1,3,2};
-        var result = Sort.sort(array);
-        assertArrayEquals(new int[]{1,2,3}, result);
+        int[] array = new int[]{1, 3, 2};
+        int[] result = Sort.sort(array);
+        assertArrayEquals(new int[]{1, 2, 3}, result);
     }
 }
