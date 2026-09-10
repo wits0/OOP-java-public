f0d73f403f9c964bcc942c00a41adbd4b1e494a5
wits0 <v.bobrovnikov@g.nsu.ru>
2026-09-10 11:32:56 +0700

    Task_1_1_1 (#5)
    * add sort method
    
    * fix gradle
    
    * fix checkstyle!!!
    
    * fix checkstyle AGAIN !!!
    
    * rename func(i'm checking my commits)
    
    * done codding
    
    * second try
    
    * popka!
    
    * popka!
    
    * mazafaka
    
    * add heapify tests
    
    * deleted heapUp, cuz don't need it, probably it may help

---
+++ Task_1_1_1/src/main/java/ru/nsu/wits/task_1_1_1/HeapSort.java
@@ -0,0 +1,61 @@
+package ru.nsu.wits.task_1_1_1;
+
+/**
+ * Heap sort realization.
+ * Copyrighting by @wits0 2026 ez so ez task for me omg
+ * Dota 2 6200mmr coach!!!
+ */
+public class HeapSort {
+    /**
+     * Sorts the given array with using heapsort algorithm.
+     *
+     * @param array the input array to be sorted
+     *               return a new sorted array
+     */
+    public static void sort(int[] array) {
+        if (array == null || array.length <= 1) {
+            return;
+        }
+        int len = array.length;
+        for (int index = len / 2 - 1; index >= 0; index--) {
+            heapify(array, len, index);
+        }
+        for (int index = len - 1; index > 0; index--) {
+            swap(array, 0, index);
+            heapify(array, index, 0);
+        }
+    }
+
+    /**
+     * Heapify return properties of Heap, used swap and heapify (recursively)
+     * check that father > then his child's if not, we swap child who bigger
+     * than father(use swap) then heapifies the affected subtree.
+     *
+     * @param array the array we need to heapify
+     * @param len the length of our array
+     * @param index the root index of the subtree to heapify
+     */
+    public static void heapify(int[] array, int len, int index) {
+        int left = 2 * index + 1;
+        int right = 2 * index + 2;
+        int biggest = index;
+
+        if (left < len && array[left] > array[biggest]) {
+            biggest = left;
+        }
+        if (right < len && array[right] > array[biggest]) {
+            biggest = right;
+        }
+        if (biggest != index) {
+            swap(array, index, biggest);
+            heapify(array, len, biggest);
+        }
+    }
+
+    private static void swap(int[] array, int left, int right) {
+        int temp = array[left];
+        array[left] = array[right];
+        array[right] = temp;
+    }
+
+}
---
+++ Task_1_1_1/src/test/java/ru/nsu/wits/task_1_1_1/HeapSortTest.java
@@ -0,0 +1,77 @@
+package ru.nsu.wits.task_1_1_1;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class HeapSortTest {
+
+    @Test
+    void testSortWithArray() {
+        int[] array = {5, 2, 7, 1, 6, 3};
+        int[] expected = {1, 2, 3, 5, 6, 7};
+        HeapSort.sort(array);
+        assertArrayEquals(expected, array);
+    }
+
+    @Test
+    void testSortWithEmptyArray() {
+        int[] array = {};
+        HeapSort.sort(array);
+        assertArrayEquals(new int[]{}, array);
+    }
+
+    @Test
+    void testSortWithSingle() {
+        int[] array = {67};
+        HeapSort.sort(array);
+        assertArrayEquals(new int[]{67}, array);
+    }
+
+    @Test
+    void testSortWithSortedArray() {
+        int[] array = {1, 2, 3, 4, 5};
+        int[] expected = {1, 2, 3, 4, 5};
+        HeapSort.sort(array);
+        assertArrayEquals(expected, array);
+    }
+
+    @Test
+    void testSortWithReverseArray() {
+        int[] array = {5, 4, 3, 2, 1};
+        int[] expected = {1, 2, 3, 4, 5};
+        HeapSort.sort(array);
+        assertArrayEquals(expected, array);
+    }
+
+    @Test
+    void testSortWithDuplicates() {
+        int[] array = {3, 1, 4, 1, 5, 9, 2, 6, 5};
+        int[] expected = {1, 1, 2, 3, 4, 5, 5, 6, 9};
+        HeapSort.sort(array);
+        assertArrayEquals(expected, array);
+    }
+
+    @Test
+    void testSortWithNegativeNumbers() {
+        int[] array = {-5, -2, -8, -1, -9, -3};
+        int[] expected = {-9, -8, -5, -3, -2, -1};
+        HeapSort.sort(array);
+        assertArrayEquals(expected, array);
+    }
+
+    @Test
+    void testNull() {
+        int[] array = null;
+        HeapSort.sort(array);
+    }
+
+    @Test
+    void testHeapify2() {
+        int[] array = {3, 1, 2};
+        HeapSort.heapify(array, 3, 0);
+        assertTrue(array[0] >= array[1] && array[0] >= array[2]);
+    }
+}
